c4c689b4653de06b65402c5ccf053c8e1d7d00ab
Eduardo Santana <dudu_lima.11@hotmail.com>
2023-08-21 23:51:24 -0300

    first commit

---
+++ Empresa.Ui.Windows/incluirCliente.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Empresa.Ui.Windows
+{
+    partial class incluirCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idLabel = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.labelTel = new System.Windows.Forms.Label();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(43, 57);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(18, 13);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "ID";
+            this.idLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(280, 57);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(35, 13);
+            this.labelNome.TabIndex = 1;
+            this.labelNome.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(321, 135);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(229, 20);
+            this.textBoxEmail.TabIndex = 3;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(321, 54);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(229, 20);
+            this.textBoxNome.TabIndex = 4;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(280, 138);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(32, 13);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "Email";
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Location = new System.Drawing.Point(84, 135);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(127, 20);
+            this.textBoxTel.TabIndex = 6;
+            // 
+            // labelTel
+            // 
+            this.labelTel.AutoSize = true;
+            this.labelTel.Location = new System.Drawing.Point(43, 138);
+            this.labelTel.Name = "labelTel";
+            this.labelTel.Size = new System.Drawing.Size(39, 13);
+            this.labelTel.TabIndex = 7;
+            this.labelTel.Text = "Celular";
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(368, 242);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(182, 37);
+            this.btnIncluir.TabIndex = 8;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(46, 242);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(182, 37);
+            this.btnVoltar.TabIndex = 9;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // incluirCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 340);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.labelTel);
+            this.Controls.Add(this.textBoxTel);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.idLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "incluirCliente";
+            this.Text = "Inclusao de Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxTel;
+        private System.Windows.Forms.Label labelTel;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnVoltar;
+    }
+}
---
+++ Empresa.Ui.Windows/principalForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Empresa.Ui.Windows
+{
+    public partial class principalForm : Form
+    {
+        public principalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void arquivosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void novoClienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            incluirCliente f = new incluirCliente();
+            f.ShowDialog();
+        }
+    }
+}
